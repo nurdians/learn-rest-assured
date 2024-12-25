a66b8fa812624fa34281533c4484e72b8064db72
nurdians <nurdian.setyawan@bukalapak.com>
2024-12-25 17:43:27 +0700

    add other test scenario

--- src/test/java/testSuite/PostTest.java
+++ src/test/java/testSuite/PostTest.java
@@ -1,14 +1,22 @@
 package testSuite;
 
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.*;
 
 public class PostTest {
-    static {
+//    static {
+//        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+//    }
+    @BeforeClass
+    public void beforeClass() {
+        System.out.println("jalankan ini dulu");
         RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
     }
 
@@ -36,7 +44,49 @@ public class PostTest {
                 .then()
                 .statusCode(200)
                 .body(matchesJsonSchemaInClasspath("schemas/getResponseSchema.json"));
-
     }
 
+    @Test
+    public void createPostData() {
+        given()
+                .headers("Content-Type","application/json")
+                .body("{\"title\": \"foo\", \"body\": \"bar\", \"userId\": 1}")
+                .log().all()
+                .when()
+                .post("/posts")
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(201)
+        ;
+    }
+
+    @Test
+    public void putPostData() {
+        given()
+                .headers("Content-Type", "application/json")
+                .body("{\"title\": \"foo1\", \"body\": \"bar\", \"userId\": 1}")
+                .when()
+                .put("posts/1")
+                .then()
+                .log().all()
+                .statusCode(200)
+                ;
+    }
+
+    @Test
+    public void patchPostData() {
+//        given()
+//                .contentType("application/json")
+//                .body("{\"title\": \"foo1\"}")
+//                .when()
+//                .patch("posts/1")
+//                .then()
+//                .log().all()
+//                .assertThat()
+//                .statusCode(200)
+//        ;
+        Response response = get("/posts/1");
+        System.out.println(response.body().jsonPath().getString("title"));
+    }
 }
